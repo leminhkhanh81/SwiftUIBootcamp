21a3bef83eac624e7652bce1487bbb2870063afc
leminhkhanh81 <khanhle.t3@gmail.com>
2022-04-12 17:49:30 +0700

    Initial Commit

---
+++ SwiftUIBootcamp/SwiftUIBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBootcampApp.swift
+//  SwiftUIBootcamp
+//
+//  Created by Le Minh Khanh on 12/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
